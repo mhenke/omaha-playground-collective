73202fc121db48872beb8a3a10dea5712948a6e3
Mike Henke <henke.mike@gmail.com>
2023-10-24 22:46:19 -0500

    added side section

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,65 +1,44 @@
 const Sidebar = () => {
   return (
     <aside className="col-span-4 flex flex-col border-r border-gray-300">
-      <ul className="menu min-h-full">
-        <li>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-            Item 2
-          </a>
-        </li>
-        <li>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            Item 1
-          </a>
-        </li>
-        <li>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-              />
-            </svg>
-            Item 3
-          </a>
-        </li>
-      </ul>
+      <section className="col-span-4 flex flex-col items-center space-y-8 p-10">
+        <div className="grid grid-cols-12 rounded-xl bg-gradient-to-br from-gray-200 via-gray-200/50 to-gray-300 p-8">
+          <div className="col-span-9 grid grid-cols-1 gap-y-4">
+            <div>
+              <div className="font-lg font-bold text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur.
+              </div>
+              <div className="text-sm font-medium text-gray-600">
+                Lorem ipsum dolor sit Lorem, ipsum.
+              </div>
+            </div>
+            <div>
+              <div className="w-fit rounded-3xl bg-gray-300 px-4 py-2 text-sm text-gray-700">
+                Get unlimited access
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 grid w-full place-items-center">
+            <i
+              className="feather feather-book"
+              style={{ fontSize: "115px", color: "white" }}
+            />
+          </div>
+        </div>
+
+        <div className="flex w-full flex-col space-y-6">
+          <div className="font-bold">People you might be interested</div>
+          <div className="flex w-full flex-col space-y-4">
+            {/* User suggestions */}
+          </div>
+        </div>
+
+        <div className="flex w-full flex-col space-y-6">
+          <div className="font-bold">My Playground list</div>
+          <div className="flex flex-col space-y-4">{/* Playground list */}</div>
+        </div>
+      </section>
     </aside>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import BlogItem from "./_components/BlogItem";
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col py-2">
-      Hello from ultimate blog app!
       {Array.from({ length: 10 }).map((_, i) => (
         <BlogItem key={`blog-item-${i}`} />
       ))}
