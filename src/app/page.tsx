74e51deec331995be77d737e80904d1c8da65173
Mike Henke <henke.mike@gmail.com>
2023-10-25 12:57:59 -0500

    fixing errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import BlogItem from "./_components/BlogItem";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="flex min-h-screen flex-col py-2">
       {Array.from({ length: 10 }).map((_, i) => (
-        <BlogItem key={`blog-item-${i}`} />
+        <BlogItem key={`blog-item-${i}`} isLastItem={i === 9} />
       ))}
     </div>
   );
