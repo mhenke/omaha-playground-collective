dbe7a405850db8c2aef0ad3d7a1133aefeaf9d78
Mike Henke <henke.mike@gmail.com>
2023-10-25 13:24:30 -0500

    fixing post.ts author

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -33,7 +33,7 @@ export const postRouter = createTRPCRouter({
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
+      where: { author: { id: ctx.session.user.id } },
     });
   }),
 
