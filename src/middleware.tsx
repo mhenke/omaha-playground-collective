a2b0dd5cfa78608b04d857afc15ae75aadcb146c
Mike Henke <henke.mike@gmail.com>
2023-12-05 22:18:07 -0600

    fixing build issue

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,21 +2,16 @@ import { withAuth, type NextRequestWithAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 console.log("hola middleware");
 
-export default withAuth(
-  function middleware(req: NextRequestWithAuth) {
-    console.log("hola req.nextUrl.pathname", req.nextUrl.pathname);
-    console.log("hola req?.nextauth?.token?.role", req?.nextauth?.token?.role);
+export default withAuth(function middleware(req: NextRequestWithAuth) {
+  console.log("hola req.nextUrl.pathname", req.nextUrl.pathname);
+  console.log("hola req?.nextauth?.token?.role", req?.nextauth?.token?.role);
 
-    if (
-      req.nextUrl.pathname.startsWith("/admin") &&
-      req?.nextauth?.token?.role != "admin"
-    ) {
-      return NextResponse.rewrite(new URL("/denied", req.url));
-    }
-  },
-  {
-    secret: process.env.NEXTAUTH_SECRET,
-  },
-);
+  if (
+    req.nextUrl.pathname.startsWith("/admin") &&
+    req?.nextauth?.token?.role != "admin"
+  ) {
+    return NextResponse.rewrite(new URL("/denied", req.url));
+  }
+});
 console.log("hola middleware 2");
 export const config = { matcher: ["/admin"] };
