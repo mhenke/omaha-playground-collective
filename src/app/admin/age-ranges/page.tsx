ec98e81bb2810e16985459bfada5c49735713eca
Mike Henke <henke.mike@gmail.com>
2023-12-07 10:38:44 -0600

    server actions working

--- src/app/admin/age-ranges/page.tsx
+++ src/app/admin/age-ranges/page.tsx
@@ -1,62 +1,32 @@
-"use client";
+import { revalidatePath } from "next/cache";
+import { api } from "~/trpc/server";
+import AddButton from "./AddButton";
+const todos = ["Learn React"];
 
-import { useState } from "react";
+const AgeRangesPage = async () => {
+  const ageRanges = await api.ageRange.getAll.query();
 
-const AgeRangesPage = ({}) => {
-  const [name, setName] = useState("");
+  console.log(ageRanges);
 
-  const handleCreate = async (event: { preventDefault: () => void }) => {
-    event.preventDefault();
+  async function addAgeRange(todo: string) {
+    "use server";
 
-    try {
-      // Use the API route for form submission
-      const response = await fetch("/api/ageRange/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name }),
-      });
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-      if (response.ok) {
-        // Optionally, handle success on the client side
-        // Reload the page or update the local state as needed
-        console.log("Form submitted successfully");
-      } else {
-        // Handle server-side errors
-        console.error("Error submitting form:", response.statusText);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        // Now TypeScript knows that `error` is an instance of `Error`
-        console.error("Error submitting form:", error.message);
-      } else {
-        // If it's not an `Error`, we can't be sure what properties it has
-        console.error("An error occurred:", error);
-      }
-    }
-  };
+    todos.push(todo);
+    revalidatePath("/admin/age-ranges");
+  }
 
   return (
-    <div className="p-4">
-      {/* ... existing code ... */}
-      <form className="mt-4" onSubmit={handleCreate}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          className="mr-2 rounded border border-gray-300 p-2"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="rounded bg-green-500 px-2 py-1 text-white"
-        >
-          Create
-        </button>
-      </form>
-    </div>
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">Todos</h1>
+      <ul>
+        {ageRanges.map((ageRangeItem, _index) => (
+          <li key={ageRangeItem.id}>{ageRangeItem.name}</li>
+        ))}
+      </ul>
+      <AddButton addTodo={addAgeRange} />
+    </main>
   );
 };
 
