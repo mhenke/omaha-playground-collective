bda2bede915720e5ef1b17bce05798f92a2268b6
Mike Henke <henke.mike@gmail.com>
2023-12-10 12:25:31 -0600

    adding fields to post

---
+++ src/app/admin/playgrounds/page.tsx
@@ -0,0 +1,386 @@
+"use client";
+
+import { type Post } from "@prisma/client";
+import { useState } from "react";
+import Loading from "~/app/_components/Loading";
+import { api } from "~/trpc/react";
+import AdminNav from "../AdminNav";
+
+const Posts = () => {
+  const postsQuery = api.post.getAll.useQuery({}, {});
+  console.log(postsQuery);
+
+  const { data: posts } = postsQuery;
+  console.log(posts);
+
+  const playgroundsQuery = api.playground.getAll.useQuery();
+  console.log(playgroundsQuery);
+
+  const { data: playgrounds } = playgroundsQuery;
+  console.log(playgrounds);
+
+  const ageRangesQuery = api.ageRange.getAll.useQuery();
+  console.log(ageRangesQuery);
+
+  const { data: ageRanges } = ageRangesQuery;
+  console.log(ageRanges);
+
+  const surfacesQuery = api.surface.getAll.useQuery();
+  console.log(surfacesQuery);
+
+  const { data: surfaces } = surfacesQuery;
+  console.log(surfaces);
+
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [rating, setRating] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [restrooms, setRestrooms] = useState(false);
+  const [picnicAreas, setPicnicAreas] = useState(false);
+  const [benches, setBenches] = useState(false);
+  const [shade, setShade] = useState(false);
+  const [accessibleEquip, setAccessibleEquip] = useState(false);
+  const [adaCompliance, setAdaCompliance] = useState(false);
+
+  const resetFormState = () => {
+    setTitle("");
+    setContent("");
+    setRating("");
+    setAddress("");
+    setCity("");
+    setState("");
+    setZip("");
+    setLatitude(null);
+    setLongitude(null);
+    setRestrooms(false);
+    setPicnicAreas(false);
+    setBenches(false);
+    setShade(false);
+    setAccessibleEquip(false);
+    setAdaCompliance(false);
+  };
+
+  const handleCreateSuccess = async () => {
+    resetFormState();
+    handleCloseDialog();
+    await postsQuery.refetch();
+  };
+
+  const createPostMutation = api.post.create.useMutation({
+    onSuccess: handleCreateSuccess,
+  });
+
+  const updatePostMutation = api.post.update.useMutation({
+    onSuccess: async () => {
+      handleCloseDialog();
+      await postsQuery.refetch();
+    },
+  });
+
+  const deletePostMutation = api.post.delete.useMutation({
+    onSuccess: () => postsQuery.refetch,
+  });
+
+  const handleDeletePost = (id: number) => {
+    deletePostMutation.mutate({ id });
+  };
+
+  const handleUpdatePost = () => {
+    if (selectedPost) {
+      updatePostMutation.mutate({
+        id: selectedPost.id,
+        title,
+        content,
+      });
+    } else {
+      createPostMutation.mutate({
+        title,
+        content,
+      });
+    }
+  };
+
+  const handleCloseDialog = () => {
+    document.getElementById("edit_modal")?.classList.remove("modal-open");
+  };
+
+  const openEditModal = (post: Post | null) => {
+    setSelectedPost(post);
+    setTitle(post ? post.title : "");
+    setContent(post ? post.content : "");
+
+    // Set other fields as needed
+    document.getElementById("edit_modal")?.classList.add("modal-open");
+  };
+
+  return (
+    <div>
+      <AdminNav />
+      <div className="min-h-screen px-4 py-4 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-2">
+        <div className="flex flex-col sm:mx-auto sm:max-w-full md:max-w-full lg:max-w-full">
+          <h1 className="mb-4 text-3xl font-bold">Manage Posts</h1>
+          <div className="min-w-full">
+            <button
+              className="btn btn-primary mb-4 flex justify-end"
+              onClick={() => openEditModal(null)}
+            >
+              Add Post
+            </button>
+          </div>
+          {postsQuery.isLoading ? (
+            <Loading />
+          ) : (
+            <table className="min-w-full border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="border p-2">Title</th>
+                  <th className="border p-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {posts?.map((postItem) => (
+                  <tr key={postItem.id}>
+                    <td className="border p-2">{postItem.title}</td>
+                    <td className="border p-2">
+                      <button
+                        className="btn btn-sm m-1"
+                        onClick={() => openEditModal(postItem)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-error btn-sm m-1"
+                        onClick={() => handleDeletePost(postItem.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+          <dialog
+            id="edit_modal"
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <form method="dialog" className="flex justify-end">
+                <button
+                  className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+                  onClick={handleCloseDialog}
+                >
+                  ✕
+                </button>
+              </form>
+              <h3 className="text-lg font-bold">
+                {selectedPost ? "Edit Post" : "Add New Post"}
+              </h3>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Content
+                </label>
+                <textarea
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Surface
+                </label>
+                <select
+                  value={selectedSurface}
+                  onChange={(e) => setSelectedSurface(e.target.value)}
+                  className="select mt-1 w-full max-w-xs rounded border p-2"
+                >
+                  <option disabled value="" selected>
+                    Pick a surface
+                  </option>
+                  {surfaces.map((surface) => (
+                    <option key={surface.id} value={surface.id}>
+                      {surface.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="py-4">
+                <p>Author Name: NAME GOES HERE</p>
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  City
+                </label>
+                <input
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  State
+                </label>
+                <input
+                  type="text"
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  ZIP
+                </label>
+                <input
+                  type="text"
+                  value={zip}
+                  onChange={(e) => setZip(e.target.value)}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Latitude
+                </label>
+                <input
+                  type="number"
+                  value={latitude}
+                  onChange={(e) => setLatitude(parseFloat(e.target.value))}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Longitude
+                </label>
+                <input
+                  type="number"
+                  value={longitude}
+                  onChange={(e) => setLongitude(parseFloat(e.target.value))}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Restrooms
+                </label>
+                <input
+                  type="checkbox"
+                  checked={restrooms}
+                  onChange={() => setRestrooms(!restrooms)}
+                  className="mt-1"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Picnic Areas
+                </label>
+                <input
+                  type="checkbox"
+                  checked={picnicAreas}
+                  onChange={() => setPicnicAreas(!picnicAreas)}
+                  className="mt-1"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Benches
+                </label>
+                <input
+                  type="checkbox"
+                  checked={benches}
+                  onChange={() => setBenches(!benches)}
+                  className="mt-1"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Shade
+                </label>
+                <input
+                  type="checkbox"
+                  checked={shade}
+                  onChange={() => setShade(!shade)}
+                  className="mt-1"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Accessible Equipment
+                </label>
+                <input
+                  type="checkbox"
+                  checked={accessibleEquip}
+                  onChange={() => setAccessibleEquip(!accessibleEquip)}
+                  className="mt-1"
+                />
+              </div>
+              <div className="py-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  ADA Compliance
+                </label>
+                <input
+                  type="checkbox"
+                  checked={adaCompliance}
+                  onChange={() => setAdaCompliance(!adaCompliance)}
+                  className="mt-1"
+                />
+              </div>
+
+              <div className="modal-action">
+                <form method="dialog">
+                  <div className="flex justify-end">
+                    <button className="btn" onClick={handleCloseDialog}>
+                      Cancel
+                    </button>
+                    <button
+                      className="btn btn-primary"
+                      onClick={handleUpdatePost}
+                    >
+                      {selectedPost ? "Update" : "Save"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
---
+++ src/server/api/routers/playground.ts
@@ -0,0 +1,120 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const playgroundRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        post: z.number(),
+        rating: z.number().nullable(),
+        address: z.string().nullable(),
+        city: z.string().nullable(),
+        state: z.string().nullable(),
+        zip: z.string().nullable(),
+        latitude: z.number().nullable(),
+        longitude: z.number().nullable(),
+        restrooms: z.boolean().nullable(),
+        picnicAreas: z.boolean().nullable(),
+        benches: z.boolean().nullable(),
+        shade: z.boolean().nullable(),
+        accessibleEquip: z.boolean().nullable(),
+        adaCompliance: z.boolean().nullable(),
+        // Include other required fields from the Playground model
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.playground.create({
+        data: {
+          name: input.name,
+          post: input.post,
+          rating: input.rating,
+          address: input.address,
+          city: input.city,
+          state: input.state,
+          zip: input.zip,
+          latitude: input.latitude,
+          longitude: input.longitude,
+          restrooms: input.restrooms,
+          picnicAreas: input.picnicAreas,
+          benches: input.benches,
+          shade: input.shade,
+          accessibleEquip: input.accessibleEquip,
+          adaCompliance: input.adaCompliance,
+          // Map other fields from the input
+          authorId: ctx.session.user.id,
+        },
+      });
+    }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.playground.findMany({
+      // You can include orderBy or any other filtering options here
+      orderBy: { createdAt: "desc" },
+      include: {
+        surface: true,
+        ageRange: true,
+        post: true,
+      },
+    });
+  }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string().min(1),
+        post: z.number(),
+        rating: z.number().nullable(),
+        address: z.string().nullable(),
+        city: z.string().nullable(),
+        state: z.string().nullable(),
+        zip: z.string().nullable(),
+        latitude: z.number().nullable(),
+        longitude: z.number().nullable(),
+        restrooms: z.boolean().nullable(),
+        picnicAreas: z.boolean().nullable(),
+        benches: z.boolean().nullable(),
+        shade: z.boolean().nullable(),
+        accessibleEquip: z.boolean().nullable(),
+        adaCompliance: z.boolean().nullable(),
+        // Include other fields that can be updated
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.playground.update({
+        where: { id: input.id },
+        data: {
+          name: input.name,
+          post: input.post,
+          rating: input.rating,
+          address: input.address,
+          city: input.city,
+          state: input.state,
+          zip: input.zip,
+          latitude: input.latitude,
+          longitude: input.longitude,
+          restrooms: input.restrooms,
+          picnicAreas: input.picnicAreas,
+          benches: input.benches,
+          shade: input.shade,
+          accessibleEquip: input.accessibleEquip,
+          adaCompliance: input.adaCompliance,
+          // Map other fields from the input
+          authorId: ctx.session.user.id,
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.playground.delete({
+        where: { id: input.id },
+      });
+    }),
+});
