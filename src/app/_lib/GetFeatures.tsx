103ce2ddddc7cd87d9acc82763d17d58b696adde
Mike Henke <henke.mike@gmail.com>
2023-12-28 14:34:53 -0600

    clean up

---
+++ src/app/_lib/GetFeatures.tsx
@@ -0,0 +1,45 @@
+import { type AgeRange, type Playground, type Surface } from "@prisma/client";
+import { useFilterStore } from "../_store/filterStore";
+
+type ExtendedPlayground = Playground & {
+  ageRange: AgeRange | null; // Add ageRange property
+  surface: Surface | null; // Add Surface property
+};
+
+export function getFeatures(playground: ExtendedPlayground) {
+  const { keys } = useFilterStore.getState();
+
+  const features = Object.fromEntries(
+    Object.entries(keys)
+      .filter(
+        ([key]) =>
+          playground[key as keyof Playground] !== null &&
+          playground[key as keyof Playground] !== undefined &&
+          playground[key as keyof Playground] === true,
+      )
+      .map(([key, { displayName, color }]) => [
+        key,
+        { displayName, value: playground[key as keyof Playground], color },
+      ]),
+  );
+
+  // Add ageRange value
+  if (playground?.ageRange?.name) {
+    features["ageRange" as keyof typeof features] = {
+      displayName: "Age Range",
+      value: playground.ageRange.name,
+      color: "primary",
+    };
+  }
+
+  // Add Surface value
+  if (playground?.surface?.name) {
+    features["surface" as keyof typeof features] = {
+      displayName: "Surface",
+      value: playground?.surface?.name,
+      color: "secondary",
+    };
+  }
+
+  return features;
+}
